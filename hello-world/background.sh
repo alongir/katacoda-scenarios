b406320db0752ba8956060c789624d633415a2d9
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-27 10:39:20 -0800

    Update background.sh

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -2,5 +2,5 @@
 
 echo "Installing latest version of Helm"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
+# minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
 echo "done" >> /root/katacoda-finished
