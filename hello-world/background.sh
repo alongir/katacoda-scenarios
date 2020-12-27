db05a650682c2be36b0481104cec56a423d5c408
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-26 23:17:23 -0800

    Update background.sh

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -2,7 +2,7 @@
 
 echo "Installing latest version of Helm"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
+minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
 # sleep 10
 
 echo "done" >> /opt/.backgroundfinished
