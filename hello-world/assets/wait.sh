82ada9a15704bb734f8ab3c263b1a95e8e2bcc64
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-26 23:54:44 -0800

    Update wait.sh

--- hello-world/assets/wait.sh
+++ hello-world/assets/wait.sh
@@ -21,23 +21,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Started"
-  echo -n "Configuring"
-  while true; do 
-    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
-    if [[ "$?" -ne 0 ]]; then     
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "Configured"
+  echo -n "Done"
 }
 
 show_progress
