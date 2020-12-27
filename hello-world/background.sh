7e8de2497277e14150298c72d49d528e6e20de88
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-27 00:01:04 -0800

    Update background.sh

--- hello-world/background.sh
+++ hello-world/background.sh
@@ -3,9 +3,8 @@
 echo "Installing latest version of Helm"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
-echo "done" >> /root/katacoda-minikube-finished
 git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
 kubectl apply -f ./microservices-demo/release/kubernetes-manifests.yaml
 # sleep 10
 
-echo "done" >> /root/katacoda-app-finished
+echo "done" >> /root/katacoda-finished
