d266397cc63881a7be7d6f9c22d126af26dc807e
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-26 23:18:23 -0800

    Update foreground.sh

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-printf "Upgrading Helm and running Minikube\n" 
+printf "Waiting for enviornmnet setup\n" 
 while [ ! -f /opt/.backgroundfinished ] ; do sleep 1; printf "|"; done; 
-printf "\nStarting Minikube\n"; minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
-printf "Ready!"
+printf "\nReady!\n"
