aa2cd78a2dd4b3d0c40b7d0caa1dd917499fa72a
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-26 20:58:25 -0800

    Update foreground.sh

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -1,1 +1,4 @@
-echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
+#!/bin/bash
+
+echo "Waiting to complete"; 
+while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
