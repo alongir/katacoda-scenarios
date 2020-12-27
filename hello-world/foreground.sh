13fa32505019f2ea9808fe0d1e1c300d25058d74
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-26 23:28:11 -0800

    Update foreground.sh

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "Waiting for enviornmnet setup\n"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 1; printf "|"; done; printf "\nReady!\n"
+printf "\rWaiting for enviornmnet setup "; while [ ! -f /opt/.backgroundfinished ] ; do sleep 1; printf "|"; done; printf " Ready!\n"
