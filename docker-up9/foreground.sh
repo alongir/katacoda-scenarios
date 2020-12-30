40cbd0581113d58bf4bd400dcd27daffacf35943
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2020-12-30 14:24:58 -0800

    Update foreground.sh

--- docker-up9/foreground.sh
+++ docker-up9/foreground.sh
@@ -1,1 +1,4 @@
 sleep 5; wait.sh
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
+export UP9_CREDENTIALS=XtEtVPVHqNr021W4ZvflxWlturB5KK6G:rwl01oL9L_0W51oygUgTfnrdfQUJ9APG:up9.app
+export WORKSPACE=workspace$RANDOM
