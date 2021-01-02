79f85f4cc72abae52e7a8fa530736d3e7f64b442
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2021-01-01 20:37:42 -0800

    Update background.sh

--- docker-up9/background.sh
+++ docker-up9/background.sh
@@ -3,9 +3,9 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo 'export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
+echo "export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
 export RND=$RANDOM
 export WORKSPACE=workspace$RND
-export AGENT=agent$RND' >> /root/.up9-config
+export AGENT=agent$RND" >> /root/.up9-config
   
 echo "done" >> /root/katacoda-finished
