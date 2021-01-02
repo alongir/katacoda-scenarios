617b4e57b4a1c1f2162dcbbeef249ee7243862a1
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2021-01-01 20:50:10 -0800

    Update step3-verify.sh

--- docker-up9/step3-verify.sh
+++ docker-up9/step3-verify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /root/.up9-config
 if [ $(up9 agent:list|grep $AGENT|wc -l) -eq 1 ]; then
-  echo "done"
+  if [ $(up9 agent:list|grep $AGENT|grep undefined| wc -l) -eq 0 ]; then
+    echo "done"
+  fi
 fi
