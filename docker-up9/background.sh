6d59a61a31b66e2a0bc2c0efda607fb9032b3356
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2021-01-01 20:23:47 -0800

    Update background.sh

--- docker-up9/background.sh
+++ docker-up9/background.sh
@@ -3,16 +3,9 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-#echo "Installing latest version of Helm"
-#curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-#minikube start; minikube addons enable dashboard; kubectl apply -f /opt/kubernetes-dashboard.yaml
-#git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
-#kubectl apply -f ./microservices-demo/release/kubernetes-manifests.yaml
-# sleep 10
-
-#docker image pull gcr.io/up9-docker-hub/passive-tapper/master:latest
-#docker image pull gcr.io/up9-docker-hub/collector/master:latest
-#docker image pull gcr.io/up9-docker-hub/authentication-helper/master:latest
-#docker image pull gcr.io/up9-docker-hub/trcc-agent/master:latest
+echo 'export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
+export RND=$RANDOM
+export WORKSPACE=workspace$RND
+export AGENT=agent$RND' >> /root/.up9-config
   
 echo "done" >> /root/katacoda-finished
