cb8d06ff94909d454bc766d8b2b502eed9cb174e
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2021-01-01 20:24:24 -0800

    Update foreground.sh

--- docker-up9/foreground.sh
+++ docker-up9/foreground.sh
@@ -1,6 +1,2 @@
 sleep 5; wait.sh
-export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
-#export UP9_CREDENTIALS=XtEtVPVHqNr021W4ZvflxWlturB5KK6G:rwl01oL9L_0W51oygUgTfnrdfQUJ9APG:up9.app
-export RND=$RANDOM
-export WORKSPACE=workspace$RND
-export AGENT=agent$RND
+source /root/.up9-config
